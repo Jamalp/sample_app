83dcd72359881dc4d00be2595195d679aa7aa8fd
Jamal Powell <jamalkpowell@gmail.com>
2013-07-14 17:57:51 -0400

    finish Static Pagss:

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home" => 'static_pages#home'
   get "static_pages/help" => 'static_pages#help'
+  get "static_pages/about"
+  get "static_pages/contact"
 end
